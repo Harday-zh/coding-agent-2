4194a1caf367ca59452d536d625cc6eecc109f6d
周辉 <01583199@yto.net.cn>
2025-11-07 15:23:53 +0800

    ...

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import StudentManage from '../views/StudentManage.vue'
 import Visualization from '../views/Visualization.vue'
+import SelfModifyingCounter from '../components/SelfModifyingCounter.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/student' },
   { path: '/student', name: 'StudentManage', component: StudentManage },
-  { path: '/visualization', name: 'Visualization', component: Visualization }
+  { path: '/visualization', name: 'Visualization', component: Visualization },
+  { path: '/counter', name: 'SelfModifyingCounter', component: SelfModifyingCounter }
 ]
 
 const router = new VueRouter({
